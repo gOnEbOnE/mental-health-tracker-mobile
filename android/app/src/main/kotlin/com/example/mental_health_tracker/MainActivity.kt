b9b224bc3ca1c068dff7cc9f47dfb16de8d974ef
gOnEbOnE <proaielaeu@gmail.com>
2024-11-06 12:59:12 +0700

    tutorial 7

--- android/app/src/main/kotlin/com/example/mental_health_tracker/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/mental_health_tracker/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.mental_health_tracker_mobile
+package com.example.mental_health_tracker
 
 import io.flutter.embedding.android.FlutterActivity
 
--- windows/runner/main.cpp
+++ windows/runner/main.cpp
@@ -27,7 +27,7 @@ int APIENTRY wWinMain(_In_ HINSTANCE instance, _In_opt_ HINSTANCE prev,
   FlutterWindow window(project);
   Win32Window::Point origin(10, 10);
   Win32Window::Size size(1280, 720);
-  if (!window.Create(L"mental_health_tracker_mobile", origin, size)) {
+  if (!window.Create(L"mental_health_tracker", origin, size)) {
     return EXIT_FAILURE;
   }
   window.SetQuitOnClose(true);
